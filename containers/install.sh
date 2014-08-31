2422fae52647112b22b75d993a4d6b5e7a831de6
Alexey Sharov <www.pismeco@gmail.com>
2014-08-31 15:11:03 +0700

    [docker] spand hdd space instead of my time

--- containers/install.sh
+++ containers/install.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-docker run --name mysql -p 3306:3306 -v /var/lib/mysql:/var/lib/mysql -d nizsheanez/mysql
+
+#Mysql
+docker run --name mysql \
+        -p 3306:3306 \
+        -v /var/lib/mysql:/var/lib/mysql \
+        -v /etc/mysql:/etc/mysql \
+        -d nizsheanez/mysql
+
+#Golang
+docker run --name mysql \
+        -v /var/lib/mysql:/var/lib/mysql \
+        -v /etc/mysql:/etc/mysql \
+        -d nizsheanez/mysql
